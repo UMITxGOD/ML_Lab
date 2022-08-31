5dae9a313f661f2eebe9f7c7fde9933bd95de1bd
umit <umitkumarsahoo111@gmail.com>
2022-08-31 22:29:14 +0530

    upto iterator

--- First_Lab_18_07_2022/E1_Q1.py
+++ First_Lab_18_07_2022/E1_Q1.py
@@ -1,6 +1,12 @@
-Name=str(input("Enter Your Name : "))
-Reg_Num=int(input("Enter Your Registration Number : "))
-Roll_No=str(input("Enter Your Roll_No : "))
-Branch=str(input("Enter Your Branch : "))
-
-print("Name :",Name,"\n Registration Number :",Reg_Num,"\n Roll Number : ",Roll_No,"\n Branch : ",Branch)
+"""
+This is my first Python programme
+Welcome to Machine Learning
+"""
+Name=str(input("Enter Your Name : "))#This is My name
+Reg_Num=int(input("Enter Your Registration Number : "))#This is My Registration Number
+Roll_No=str(input("Enter Your Roll_No : "))#This is My Roll Number
+Branch=str(input("Enter Your Branch : "))#This is Branch
+semester=str(input("Enter Your semester : "))#This is semester
+department=str(input(("Enter Your Department : "))#This is My department
+college_name=str(input("Enter Your college : "))#This is My college Name
+print("Name :",Name,"\n Registration Number :",Reg_Num,"\n Roll Number : ",Roll_No,"\n Branch : ",Branch,"\n Semester : ",semester,"\n Department : ",department,"\n College Name : ",college_name)
---
+++ classificationtree/dicessionTree.py
@@ -0,0 +1,12 @@
+def absolute_value(*values):
+values[0]
+
+    if num >= 0:
+        return num
+    else:
+        return -num
+
+
+print(absolute_value(2))
+
+print(absolute_value(-4))
